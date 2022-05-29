b0044bf52a354dbcdca7694f9d3584bf3e3d1d62
Crimsontid <thebettertomorrow112@gmail.com>
2022-05-29 21:43:38 +0900

    creating env2

--- 1.py
+++ 1.py
@@ -1,3 +1,19 @@
-from django import apps
+
+# git init
+
+# git remote add origin '저장소 주소'
+
+# git add .
+
+# git commit -m "commit title"
+
+# touch README.md  # fsutil file createnew README.md 0
+# touch .gitignore  # fsutil file createnew .gitignore 0
+
+# git add .
+
+# git commit -m "second init"
+
+
 
 
---
+++ README.md
@@ -0,0 +1,3 @@
+# AirBnB Clone
+
+Cloning AirBnb with python and django
